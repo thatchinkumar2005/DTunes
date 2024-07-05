06aeee43aa0ff15ab80a9f95911adbedc5cd61bd
Thatchin Kumar <thatchinkumar2005@icloud.com>
2024-07-05 03:24:10 -0700

    SERVER: search

--- server/controllers/Search/searchController.js
+++ server/controllers/Search/searchController.js
@@ -9,7 +9,7 @@ export default async function searchController(req, res) {
     const page = queries?.page || 1;
     const limit = queries?.limit || 5;
     const q = queries?.query;
-    const type = queries?.type;
+    const type = queries?.type || "all";
     const offset = (page - 1) * limit;
     console.log(queries);
 
@@ -45,6 +45,7 @@ export default async function searchController(req, res) {
           { lname: { $regex: q, $options: "i" } },
           { username: { $regex: q, $options: "i" } },
         ],
+        "roles.artist": { $exists: true },
       })
         .skip(offset)
         .limit(limit);
@@ -55,6 +56,72 @@ export default async function searchController(req, res) {
         playlists: nameBasedPlaylists,
         artists: nameBasedArtists,
       });
+    } else if (type === "song") {
+      const nameBasedSongs = await Song.find({
+        name: { $regex: q, $options: "i" },
+      })
+        .skip(offset)
+        .limit(limit);
+
+      const genreBasedSong = await Song.find({
+        genre: { $regex: q, $options: "i" },
+      })
+        .skip(offset)
+        .limit(limit);
+
+      return res.json({
+        songs: [...nameBasedSongs, ...genreBasedSong],
+      });
+    } else if (type === "album") {
+      const nameBasedAlbums = await Album.find({
+        name: { $regex: q, $options: "i" },
+      })
+        .skip(offset)
+        .limit(limit);
+      return res.json({
+        albums: nameBasedAlbums,
+      });
+    } else if (type === "playlist") {
+      const nameBasedPlaylists = await Playlist.find({
+        name: { $regex: q, $options: "i" },
+        public: true,
+      })
+        .skip(offset)
+        .limit(limit);
+      return res.json({
+        playlists: nameBasedPlaylists,
+      });
+    } else if (type === "artist") {
+      const nameBasedArtists = await User.find({
+        $or: [
+          { fname: { $regex: q, $options: "i" } },
+          { lname: { $regex: q, $options: "i" } },
+          { username: { $regex: q, $options: "i" } },
+        ],
+        "roles.artist": { $exists: true },
+      })
+        .skip(offset)
+        .limit(limit);
+
+      return res.json({
+        artists: nameBasedArtists,
+      });
+    } else if (type === "user") {
+      const nameBasedUsers = await User.find({
+        $or: [
+          { fname: { $regex: q, $options: "i" } },
+          { lname: { $regex: q, $options: "i" } },
+          { username: { $regex: q, $options: "i" } },
+        ],
+      })
+        .skip(offset)
+        .limit(limit);
+
+      return res.json({
+        users: nameBasedUsers,
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid type" });
     }
   } catch (error) {
     return res.status(500).json({ message: error.message });
