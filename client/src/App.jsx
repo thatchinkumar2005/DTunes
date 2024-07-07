c9f25b36be965edddd55ab0c5a59f10292ee9642
Thatchin Kumar <thatchinkumar2005@icloud.com>
2024-07-07 02:45:14 -0700

    CLIENT: file structure init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,3 @@
-export default function App(){
-  return <>
-    <h1 className="text-center text-blue-400 font-bold text-5xl">Hello Dtunes</h1>
-  </>
+export default function App() {
+  return <></>;
 }
