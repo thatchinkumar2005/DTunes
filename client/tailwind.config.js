12ab76f0155ab9ab1eb92f70d6eba7c563557932
Thatchin Kumar <thatchinkumar2005@icloud.com>
2024-07-07 04:34:23 -0700

    theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: "#0C0C0C",
+        text: "#F4F9F9",
+        primary: "#526D82",
+        secondary: "#27374D",
+        button: "#F2613F",
+      },
+    },
   },
   plugins: [],
 };
