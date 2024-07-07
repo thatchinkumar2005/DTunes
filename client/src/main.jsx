985046d94766952536375ae5b4c0ef984fecdd48
Thatchin Kumar <thatchinkumar2005@icloud.com>
2024-07-07 10:24:42 -0700

    react-query and redux set up

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
 import "./index.css";
+import App from "./App.jsx";
 import AuthContextProvider from "./contexts/authContex.jsx";
+import store from "./redux/store.js";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthContextProvider>
-        <Routes>
-          <Route path="*" element={<App />} />
-        </Routes>
-      </AuthContextProvider>
+      <QueryClientProvider queryClient={queryClient}>
+        <Provider store={store}>
+          <AuthContextProvider>
+            <Routes>
+              <Route path="*" element={<App />} />
+            </Routes>
+          </AuthContextProvider>
+        </Provider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
