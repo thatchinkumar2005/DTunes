9083ecbcc328245a5be4ff3fe41d1a55c84bf13e
Thatchin Kumar <thatchinkumar2005@icloud.com>
2024-07-16 10:06:30 -0700

    SERVER: get single album s3

--- server/controllers/Albums/getSingleAlbum.js
+++ server/controllers/Albums/getSingleAlbum.js
@@ -1,4 +1,7 @@
+import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { Album } from "../../models/Album.js";
+import { s3 } from "../../config/bucketConn.js";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export default async function getSingleAlbum(req, res) {
   try {
@@ -6,7 +9,16 @@ export default async function getSingleAlbum(req, res) {
     if (!id) return res.status(400).json({ message: "No id given" });
 
     const album = await Album.findById(id);
-    res.json(album);
+
+    const command = new GetObjectCommand({
+      Bucket: process.env.BUCKET_NAME,
+      Key: `CoverArt/${album.id}.png`,
+    });
+
+    const url = await getSignedUrl(s3, command, { expiresIn: 3600 * 24 });
+    album.files.coverArt = url;
+
+    return res.json(album);
   } catch (error) {
     return res.status(500).json({
       message: error.message,
