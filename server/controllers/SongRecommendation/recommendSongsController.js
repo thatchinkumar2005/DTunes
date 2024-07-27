57507ad079c8b64a45c3b716bcb8ac043084df78
thatchinkumar2005 <thatchinkumar2005@icloud.com>
2024-07-27 12:00:54 +0530

    Get recommended on end of queue

--- server/controllers/SongRecommendation/recommendSongsController.js
+++ server/controllers/SongRecommendation/recommendSongsController.js
@@ -16,41 +16,6 @@ export default async function recommendSongsController(req, res) {
     const page = queries.page || 1;
     const limit = queries.limit || 10;
 
-    // const mostPlays = await Interaction.aggregate()
-    //   .match({ intType: "play" })
-    //   .group({
-    //     _id: "$song",
-    //     plays: { $sum: "$count" },
-    //   })
-    //   .sort({ plays: -1 })
-    //   .skip((page - 1) * limit)
-    //   .limit(limit);
-
-    // const mostLikes = await Like.aggregate()
-    //   .group({
-    //     _id: "$song",
-    //     likes: { $sum: 1 },
-    //   })
-    //   .sort({ likes: -1 })
-    //   .skip((page - 1) * limit)
-    //   .limit(limit);
-
-    // const resultIds = [...mostLikes, ...mostPlays];
-    // const result = await Promise.all(
-    //   resultIds.map(async (i) => {
-    //     const song = await Song.findOne({ _id: i._id });
-    //     console.log(song);
-    //     return song;
-    //   })
-    // );
-    // if (result.length < 2 * limit) {
-    //   const fill = await Song.find()
-    //     .sort({ releaseDate: -1 })
-    //     .limit(2 * limit - result.length);
-
-    //   return res.json([...result, ...fill]);
-    // }
-    // return res.json(result);
     const resp = await getRecommendation({
       userId: userIdStr,
       page,
--- server/controllers/Songs/nextSongController.js
+++ server/controllers/Songs/nextSongController.js
@@ -1,14 +1,36 @@
 import { User } from "../../models/User.js";
+import getRecommendation from "../../utils/getRecommendation.js";
 
 export default async function nextSongController(req, res) {
   try {
     const user = req.user;
     const userDoc = await User.findOne({ _id: user.id });
 
-    userDoc.queue.currentIndex =
-      userDoc.queue.currentIndex === userDoc.queue.currentSongs.length - 1
-        ? 0
-        : userDoc.queue.currentIndex + 1;
+    // userDoc.queue.currentIndex =
+    //   userDoc.queue.currentIndex === userDoc.queue.currentSongs.length - 1
+    //     ? 0
+    //     : userDoc.queue.currentIndex + 1;
+
+    if (userDoc.queue.currentIndex === userDoc.queue.currentSongs.length - 1) {
+      const resp = await getRecommendation({
+        userId: user.id,
+        page: 1,
+        limit: 20,
+      });
+
+      const result = JSON.parse(resp);
+
+      const songs = result.map((res) => {
+        return res.song;
+      });
+
+      userDoc.queue.currentIndex = 0;
+      userDoc.queue.currentSongs = songs;
+      userDoc.queue.clusterName = "Recommended";
+      userDoc.queue.clusterId = "/recommend";
+    } else {
+      userDoc.queue.currentIndex++;
+    }
     userDoc.queue.currentSong =
       userDoc.queue.currentSongs[userDoc.queue.currentIndex];
     await userDoc.save();
